534a78851d8889033522e102913ac43383bf0ac6
Aleksei Khudiakov <aleksey@xerkus.pro>
2016-08-18 19:25:34 +1000

    Add Protocol class abstracting connection
    Some of the intended responsibilities were stripped off protocol,
    leaving it as mere connection wrapper. Protocol now requires connected
    socket

---
+++ test/protocol.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const expect = require('chai').expect;
+const describe = require('mocha').describe;
+const it = require('mocha').it;
+// const Protocol = require('../lib/protocol');
+
+
+describe('SMPP protocol', () => {
+    it.skip('placeholder', () => {
+    });
+});
